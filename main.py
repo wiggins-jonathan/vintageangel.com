44efee4e1eda32ff3266a3731a6f173b364ae1a0
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2018-06-05 20:52:10 -0700

    Change nav links to use relative paths

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/contact.html')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
